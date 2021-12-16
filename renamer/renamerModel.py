fa5b7b9e25e40e18c4087466925958e5b941b8a4
UnPySide <satellitear@gmail.com>
2021-12-16 19:22:46 +0900

    フォルダ移動と削除

---
+++ renamer/renamerModel.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+class model(object):
+    def __init__(self, *args, **kwargs):
+        super(model, self).__init__(*args, **kwargs)
+
+    def debug(self):
+        print ("debug")
